275fbe56aa8f5c6858b6aabc8e1dc7b74fad01cc
Ricardo <146256152+ricode-icaito@users.noreply.github.com>
2023-11-08 16:26:33 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+var nombre = "Ricardo"
+var apellido = "Diosdado"
+document.write(nombre+" "+apellido)
+alert(nombre+" "+apellido)
+console.log(nombre+" "+apellido);
+document.write("<br>")
+/*
+NUMERICOS
+CADENA 
+BOLEANOS
+FECHAS
+OBJETOS
+*/
+const nume = 3.1416
+document.write(typeof  nume)
+document.write(nume)
+document.write("<br>")
+
+let num = 1234567890
+document.write(typeof  num)
+document.write(num)
+document.write("<br>")
+
+let cad = "Esto es texto"
+document.write (typeof cad)
+document.write(cad)
+document.write("<br>")
+
+let bol = true 
+document.write(typeof bol)
+document.write(bol)
+document.write("<br>")
+
+let fech = Date()
+document.write(typeof fech)
+document.write(fech)
+document.write("<br>")
+
+//operadores logicos
+let uno 
+let dos 
+let tres 
+uno = 20
+dos = 10
+tres = (uno + dos)
+document.write(tres)
+document.write("<br>")
+tres = (uno - dos)
+document.write(tres)
+document.write("<br>")
+tres = (uno * dos)
+document.write(tres)
+document.write("<br>")
+tres = (uno / dos)
+document.write(tres)
+document.write("<br>")
+tres = (uno % dos)
+document.write(tres)
+document.write("<br>")
+tres = (uno ** dos)
+document.write(tres)     
+document.write("<br>")
+++ uno 
+document.write(uno)
+document.write("<br>")
+
+let un = 20
+un --
+document.write(un)
+document.write("<br>")
+
+let una = 15+15/3
+document.write(una)
+document.write("<br>")
+
+let une = 15+15*3
+document.write(une)
+document.write("<br>")
+
+let uni = 15-15/3
+document.write(uni)
+document.write("<br>")
+
+let unu = 15-15*3
+document.write(unu)
+document.write("<br>")
+
+let u = (15+15)*3
+document.write(u)
+document.write("<br>")
+
+let num1 = 25
+let num2 = 15
+document.write(num1 <= num2)
+document.write("<br>")
+document.write(num1 >= num2)
+document.write("<br>")
+document.write(num1 == num2)
+document.write("<br>")
+document.write(num1 != num2)
